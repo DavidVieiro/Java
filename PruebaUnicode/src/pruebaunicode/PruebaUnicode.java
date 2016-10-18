4380fccf6994f769fb39ee770093c9d7d8dbaa68
DavidVieiro <davidvieiro@gmail.com>
2016-10-18 10:29:52 +0200

    Add files via upload

--- PruebaUnicode/src/pruebaunicode/PruebaUnicode.java
+++ PruebaUnicode/src/pruebaunicode/PruebaUnicode.java
@@ -12,15 +12,19 @@ public class PruebaUnicode {
     public static void main(String[] args) {
         // Definimos la variable
         char c = '€';
-        
+        // \n es un salto de linea
         System.out.println("letra -> " + (int)'A'); // Unicode 2 Bytes
-        System.out.println("letras -> " + '\u0041' + " " + '\u00C1' + "\n"); // \n es un salto de linea
+        System.out.println("letras -> " + '\u0041' + " " + '\u00C1' + "\n");
         
         System.out.println("euro -> " + (int)'€' + " " + c);
         System.out.println("euro -> " + '\u20AC' + " " + c);
         System.out.println("letras Ñ ñ -> " + (int)'Ñ' + " " + (int)'ñ' + "\n");
         
         System.out.println("secuencias escape -> " + "\"$\" " + "\\A\\");
-        System.out.println("letra Ñ -> " + '\u00D1' + " " + '\u00F1');
+        System.out.println("letra Ñ -> " + '\u00D1' + " " + '\u00F1' + "\n");
+        // Añadir el caracter &
+        System.out.println("Ampersand -> " + (int)'&' + " " + '\u0026');
+        
+        System.out.println("A letra -> " + (char)65);
     }
 }
