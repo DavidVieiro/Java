0c511305699c5c7642b78c366ba108d4773946d1
DavidVieiro <davidvieiro@gmail.com>
2016-10-18 10:45:24 +0200

    Add files via upload

---
+++ ClaseMatch/src/clasematch/ClaseMatch.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package clasematch;
+/**
+ *
+ * @author dam132
+ */
+public class ClaseMatch {
+    public static void main(String[] args) {
+        double dato;
+        
+        dato = Math.abs(-4);
+        
+        float r;
+        System.out.println("Valor absoluto -> " + dato);
+        System.out.println("El mayor es " + Math.max(99, 0));
+        System.out.println("Nº PI --> " + Math.PI);
+        System.out.println("redondeo -> " + Math.floor(90.3));
+    }
+    
+}
