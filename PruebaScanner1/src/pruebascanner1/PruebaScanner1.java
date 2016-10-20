f35d2c67224078459bf4556475164b0fb2ef95ec
DavidVieiro <davidvieiro@gmail.com>
2016-10-20 10:03:40 +0200

    Add files via upload

--- PruebaScanner1/src/pruebascanner1/PruebaScanner1.java
+++ PruebaScanner1/src/pruebascanner1/PruebaScanner1.java
@@ -17,14 +17,18 @@ public class PruebaScanner1 {
         // Se introducen los datos
         Scanner reader = new Scanner(System.in); // creacion del objeto reader
         System.out.println("Introduce la velocidad en Km/h para convertirla a m/s:");
+        
+        // http://aprenderaprogramar.es/index.php?option=com_content&view=article&id=678:gestion-de-excepciones-en-java-captura-con-bloques-try-catch-finally-ejemplos-resueltos-sencillos-cu00927c&catid=58:curso-lenguaje-programacion-java-nivel-avanzado-i&Itemid=180
         try {
             float velocidad = reader.nextFloat(); // objeto.metodo()
             velocidad = (velocidad * 1000) / 3600;
             System.out.println("La velocidad es " + velocidad + " m/s");
         }
-        catch (java.util.InputMismatchException e) {
+        // catch se ejecuta cuando encuentra algun error, el try se ejecuta linea por linea
+        catch (Exception e) { // e es para meter el error en esa variable.
             System.out.println("Datos introducidos incorrectos. Ejemplo: 50,5");
         }
+        // finally {} se ejecutaria siempre
     }
 
     
