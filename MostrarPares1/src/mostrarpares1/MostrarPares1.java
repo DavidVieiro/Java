5e65a430265738ffbc02942ad1481770df4cb412
David Vieiro Pazos <davidvieiro@gmail.com>
2016-10-24 14:24:42 +0200

    Add files via upload

---
+++ MostrarPares1/src/mostrarpares1/MostrarPares1.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mostrarpares1;
+/**
+ *
+ * @author dam132
+ */
+public class MostrarPares1 {
+    public static void main(String[] args) {
+        for (int i = 2; i != 1002; i = i + 2) {
+            System.out.println(+ i);
+        }
+    }
+    
+}
