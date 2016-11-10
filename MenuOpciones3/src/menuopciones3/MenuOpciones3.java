16fb57c1cc039399ce73b3831443dfd25308646f
David Vieiro Pazos <davidvieiro@gmail.com>
2016-11-10 09:21:54 +0100

    Add files via upload

--- MenuOpciones3/src/menuopciones3/MenuOpciones3.java
+++ MenuOpciones3/src/menuopciones3/MenuOpciones3.java
@@ -10,10 +10,16 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 /**
- * Implementar dos metodos de calculos de distinta forma, igual
- *  a) Los apartados anteriores.
- *  b) Leer los datos y mostrar el resultado del calculos.
- *  c) Leer los datos y devuelve el resultado.
+ * Menú : Cálculos
+ *   1. Valor absoluto
+ *   2. Potencia
+ *   3. Raíz cuadrada
+ *   4. Salir
+ * c) Implementar dos metodos de calculos de distinta forma, igual
+ *      Los apartados anteriores.
+ *      Leer los datos y mostrar el resultado del calculos.
+ *      Leer los datos y devuelve el resultado.
+ * d)
  */
 
 /**
@@ -22,73 +28,78 @@ import java.io.InputStreamReader;
  */
 public class MenuOpciones3 {
     
-    static int menuOpciones() {
-        return 0;
+    static int menuOpciones() throws IOException {
+        int opcion = 0;
+        
+        InputStreamReader flujo = new InputStreamReader(System.in);
+        BufferedReader teclado = new BufferedReader(flujo);
+        
+        do {
+            
+            System.out.println("Elige una opcion ( 1 - 7 ):");
+            System.out.println("- Pulse 1 - para hallar el valor absoluto de un numero.");
+            System.out.println("- Pulse 2 - para calcular la potencia de un numero.");
+            System.out.println("- Pulse 3 - para calcular la raiz cuadrada de un numero.");
+            System.out.println("- Pulse 4 - para SALIR del programa.");
+
+            try {
+                opcion = Integer.parseInt(teclado.readLine());
+            }
+            catch (NumberFormatException ex) {
+                System.out.println("Valor introducido incorrecto. Usa numeros.");
+            }
+        } while (opcion < 1 || opcion > 7);
+        return opcion;
         
     }
     
+    static double calculos () throws IOException {
+        int menu, continuar = 1;
+        double numero, numero2, n = 0;
+
+        InputStreamReader flujo = new InputStreamReader(System.in);
+        BufferedReader teclado = new BufferedReader(flujo);
+
+        do {
+            menu = menuOpciones();
+
+            switch (menu) {
+                case 1:
+                    System.out.println("Introduce un numero para devolver su valor absoluto:");
+                    numero = Double.parseDouble(teclado.readLine());
+                    n = Math.abs(numero);
+                    break;
+                case 2:
+                    System.out.println("Introduce un numero:");
+                    numero = Double.parseDouble(teclado.readLine());
+                    System.out.println("Introduce un numero");
+                    numero2 = Double.parseDouble(teclado.readLine());
+                    n = Math.pow(numero, numero2);
+                    break;
+                case 3:
+                    System.out.println("Introduce un numero para devolver su raiz cuadrada:");
+                    numero = Double.parseDouble(teclado.readLine());
+                    n = Math.sqrt(numero);
+                    break;
+                case 4:
+                    continuar = 0;
+                    break;
+                default:
+                    System.out.println("La opcion introducida no existe. Vuelve a intentarlo.");
+            }
+            return n;
+        } while ( continuar == 1 );
+    }
+    
     public static void main(String[] args) throws IOException {
-        try {
-            int menu = 7, continuar = 1;
-            double numero, numero2, n;
-            
-            InputStreamReader flujo = new InputStreamReader(System.in);
-            BufferedReader teclado = new BufferedReader(flujo);
-            
-            do {
-                //menu = menuOpciones();
-                
-                switch (menu) {
-                    case 1:
-                        System.out.println("Introduce un numero para devolver su valor absoluto:");
-                        numero = Double.parseDouble(teclado.readLine());
-                        n = Math.abs(numero);
-                        System.out.println("El valor absoluto de " + numero + " es: " + n + "\n");
-                        break;
-                    case 2:
-                        System.out.println("Introduce un numero para devolver su raiz cuadrada:");
-                        numero = Double.parseDouble(teclado.readLine());
-                        n = Math.sqrt(numero);
-                        System.out.println("La raiz cuadrada de " + numero + " es: " + n + "\n");
-                        break;
-                    case 3:
-                        System.out.println("Introduce un numero para devolver el numero entero:");
-                        numero = Double.parseDouble(teclado.readLine());
-                        n = Math.rint(numero);
-                        System.out.println("El valor entero de " + numero + " es: " + n + "\n");
-                        break;
-                    case 4:
-                        System.out.println("Introduce un numero para redondearlo hacia arriba:");
-                        numero = Double.parseDouble(teclado.readLine());
-                        n = Math.round(numero);
-                        System.out.println("El resultado del " + numero + " es: " + n + "\n");
-                        break;
-                    case 5:
-                        System.out.println("Introduce un numero:");
-                        numero = Double.parseDouble(teclado.readLine());
-                        System.out.println("Introduce un exponente para el numero anterior:");
-                        numero2 = Double.parseDouble(teclado.readLine());
-                        n = Math.pow(numero, numero2);
-                        System.out.println("El numero " + numero + " elevado a " + numero2 +" es: " + n + "\n");
-                        break;
-                    case 6:
-                        System.out.println("Introduce un numero para compararlo con el siguiente:");
-                        numero = Double.parseDouble(teclado.readLine());
-                        System.out.println("Introduce un numero para compararlo con el anterior:");
-                        numero2 = Double.parseDouble(teclado.readLine());
-                        n = Math.max(numero,numero2);
-                        System.out.println("El mayor entre " + numero + " y " + numero2 + " es: " + n + "\n");
-                        break;
-                    case 7:
-                        continuar = 0;
-                        break;
-                    default:
-                        System.out.println("La opcion introducida no existe. Vuelve a intentarlo.");
-                }
-            } while ( continuar == 1 );
-        } catch (NumberFormatException ex) {
-            System.out.println("Valor introducido incorrecto. Usa numeros.");
+        double resultado;
+        
+        resultado = calculos();
+        
+        if ( resultado != 0) {
+           System.out.println("El resultado es: " + resultado + "\n"); 
         }
+        System.out.println("Programa finalizado.");
     }
     
 }
