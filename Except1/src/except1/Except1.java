9f7f9099cd4b0a7a736d466e3b90b7c1ce85e0f8
David Vieiro Pazos <davidvieiro@gmail.com>
2016-11-21 14:03:37 +0100

    Add files via upload

---
+++ Except1/src/except1/Except1.java
@@ -0,0 +1,45 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package except1;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+/**
+ *
+ * @author dam132
+ */
+public class Except1 {
+    
+    static BufferedReader teclado = new BufferedReader( new InputStreamReader( System.in ) );
+    
+    public static void main(String[] args) throws IOException {
+        
+        String texto;
+        int i , j;
+        
+        try {
+            
+            System.out.println("Numero");
+            texto = teclado.readLine();
+            i = Integer.valueOf(texto);
+            System.out.println("Otro numero");
+            texto = teclado.readLine();
+            j = Integer.valueOf(texto);
+            System.out.println("Division " + i + " / " + j + " = " + i/j);
+            System.out.println("\nFIN DEL PROGRAMA");
+        }
+        catch (NumberFormatException ex1) {
+            System.out.println("Error de formato numerico --> " + ex1.getMessage() );
+        }
+        catch (ArithmeticException ex2) {
+            System.out.println("No se puede dividir entre 0 --> " + ex2.getMessage() );
+        }
+        
+    }
+    
+}
